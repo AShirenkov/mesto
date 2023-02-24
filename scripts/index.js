fda261f960411fdd798237b7bfaf4cda8d55fdad
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-02-24 18:41:01 +0300

    fix: JSscript Заменить двойный кавычки на одинарные

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,36 @@
-let closeButton = document.querySelector(".popup__close-button");
-let editButton = document.querySelector(".profile__edit-button");
-let saveButton = document.querySelector(".popup__save-button");
+let closeButton = document.querySelector('.popup__close-button');
+let editButton = document.querySelector('.profile__edit-button');
+let saveButton = document.querySelector('.popup__save-button');
 
-let popup = document.querySelector(".popup");
+let popup = document.querySelector('.popup');
 
-let currentName = document.querySelector(".profile__name");
-let currentDescriprion = document.querySelector(".profile__descr");
-let editName = document.querySelector(".popup__input-text_type_name");
+let currentName = document.querySelector('.profile__name');
+let currentDescriprion = document.querySelector('.profile__descr');
+let editName = document.querySelector('.popup__input-text_type_name');
 let editDescriprion = document.querySelector(
-  ".popup__input-text_type_description"
+  '.popup__input-text_type_description'
 );
 // console.log(currentName);
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
 function openPopup() {
   editName.value = currentName.textContent;
   editDescriprion.value = currentDescriprion.textContent;
 
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 function savePopup() {
   currentName.textContent = editName.value;
   currentDescriprion.textContent = editDescriprion.value;
 
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
-closeButton.addEventListener("click", closePopup);
-editButton.addEventListener("click", openPopup);
-saveButton.addEventListener("click", savePopup);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
+saveButton.addEventListener('click', savePopup);
 
-popup.addEventListener("keydown", function (e) {
+popup.addEventListener('keydown', function (e) {
   // evt.preventDefault();
   if (e.keyCode === 13) {
     savePopup();
