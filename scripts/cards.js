0f5c16eb8b6c810a223dff9c4570b20100598dce
Aleksandr Shirenkov <ashir84@yandex.ru>
2023-03-26 22:11:34 +0300

    feat: Релизовать создание карточек через класс

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,63 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
+class Card {
+  constructor(dataCard, templateSelector) {
+    this._name = dataCard.name;
+    this._link = dataCard.link;
+    this._templateSelector = templateSelector;
+    this._cardTitle = null;
+    this._cardImg = null;
+    this._removeCardButton = null;
+    this._likeCardButton = null;
+  }
+
+  _getTemplate() {
+    const cardElement = this._templateSelector.cloneNode(true);
+
+    return cardElement;
+  }
+  _likeBtnClick() {
+    this._likeCardButton.classList.toggle('card__like_active');
+  }
+  _removeCardBtnClick() {
+    this._newCard.remove();
+  }
+  _openPopupImg() {
+    popupImgPicture.src = this._link;
+    console.log(this._link);
+    popupImgPicture.alt = this._name;
+    popupImgText.textContent = this._name;
+    popupImg.classList.add('popup_opened');
+  }
+
+  _setEventListeners() {
+    this._cardImg.addEventListener('click', () => {
+      this._openPopupImg();
+    });
+
+    this._removeCardButton = this._newCard.querySelector('.card__trash-button');
+    this._removeCardButton.addEventListener('click', () => {
+      this._removeCardBtnClick();
+    });
+
+    this._likeCardButton = this._newCard.querySelector('.card__like');
+    this._likeCardButton.addEventListener('click', () => {
+      this._likeBtnClick();
+    });
+
+    /*popupCloseButton.addEventListener('click', () => {
+      this._handleClosePopup();
+    });*/
+  }
+
+  createCard() {
+    this._newCard = this._getTemplate();
+
+    this._cardTitle = this._newCard.querySelector('.card__title'); //+
+    this._cardTitle.textContent = this._name;
+
+    this._cardImg = this._newCard.querySelector('.card__img');
+    this._cardImg.src = this._link;
+    this._cardImg.alt = this._name;
+    this._setEventListeners();
+    return this._newCard;
+  }
+}
